b778a6a9ca9d1b79c528d0059e96ac03d661c2ad
Mohammed Al-Rasheed <mohammed.satar.alrasheed@gmail.com>
2022-01-01 20:59:16 -0500

    2010 s2

--- 2010/Senior/Senior2.cpp
+++ 2010/Senior/Senior2.cpp
@@ -1,11 +1,38 @@
 #include<iostream>
 #include <vector>
 #include <string>
+#include <map>
 using namespace std;
 
 
 int main() {
+    int N;
+    cin >> N;
+    std::map<string, char> dict;
+    for(int i = 0; i<N; i++) {
+        char letter;
+        string number;
+        cin >> letter >> number;
+        dict[number] = letter;
+    }
 
+    string code;
+    cin >> code;
+    int start = 0;
+    int end = 0;
+    string output;
+    while(code.size() != 0) {
+        if(dict.count(code.substr(start, end))) { 
+            output += dict[code.substr(start, end)];
+            code.erase(start, end);
+            end = 0;
+        } 
+        else {
+            end++;
+        }
+    }
+    cout << output << endl;
 }
+
 
 
